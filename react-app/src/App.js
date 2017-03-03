680d94eab6cb030d43d231fe8786ee62c82819ad
AntonEmery <antonemery@gmail.com>
2017-03-02 22:50:41 -0800

    props children

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,42 +2,23 @@ import React from 'react';
 
 /* Standard class component */
 class App extends React.Component {
-  constructor() {
-    super();
-    //define state
-    this.state = {txt: "this is the state text"}
-  }
-
-  update(e) {
-    this.setState({txt: e.target.value})
-  }
-
 
   render() {
     return (
-      <div>
-        {/*  pass in state to our component */}
-        <h1>{this.state.txt}</h1>
-        <p>{this.props.cat}</p>
-        <Widget update={this.update.bind(this)}/>
-      </div>
+      <Button>I <Heart /> React</Button>
     )
   }
 }
 
 //stateless functional component
-const Widget = (props) =>
-  <input type="text" onChange={props.update} />
+const Button = (props) =>
+  <button>{props.children}</button>
 
-
-//stateless functional Component, only returns jsx. does not have props
-//const App = () => <h1>Stateless functional component</h1>;
-
-App.propTypes = {
-  //txt prop type must be string
-  txt: React.PropTypes.string,
-  //cat prop type must be number and is required
-  cat: React.PropTypes.number.isRequired
+class Heart extends React.Component {
+  render() {
+    return <span>&hearts;</span>
+  }
 }
+
 
 export default App;
